5a745571bf1d77cda22c431106a2281524fed9fb
Christelle Baumier <christelle.baumier@epitech.eu>
2018-02-16 15:46:06 +0100

    Adding database model

--- backoffice/backoffice/app/models.py
+++ backoffice/backoffice/app/models.py
@@ -3,5 +3,15 @@ Definition of models.
 """
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Model for API keys
+
+class apiKey(models.Model):
+    api_key = models.CharField(max_length=64)
+    date_creation = models.DateTimeField(default=timezone.now)
+    date_expiration = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+         return self.api_key
