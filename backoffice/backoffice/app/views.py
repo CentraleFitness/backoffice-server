c8c07e5e1209c9332c6d5eaf92d503783f078a4c
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-03-29 16:18:19 -0700

    moved key form to same page; added delete button

--- backoffice/backoffice/app/views.py
+++ backoffice/backoffice/app/views.py
@@ -33,16 +33,35 @@ def home(request):
 
 @login_required
 def manage_key(request):
-    number_row = apiKey.objects.count()
+    if request.method == "POST":
+        form = apiKeyForm(request.POST)
+        if form.is_valid():
+            apiKey.objects.all().delete()
+            key = form.save(commit=False)
+            key.date_creation = datetime.now()
+            current_year = datetime.now().year
+            now = datetime.now()
+            key.date_expiration = datetime(current_year + 1, now.month, now.day, now.hour, now.minute, now.second)
+            key.save()
+            return redirect('manage_key')
+    else:
+        form = apiKeyForm()
     keys = apiKey.objects.all()
     return render(
         request,
         'app/manage_key.html',
         {
-            'keys' : keys,
+            'keys': keys,
+            'form': form,
             'year':datetime.now().year,
         }
     )
+
+def delete_key(request):
+    if request.method == "POST":
+        key_value = request.POST.get('api_key')
+        nb_del, _ = apiKey.objects.get(api_key=key_value).delete()
+    return redirect('manage_key')
 
 @login_required
 def new_key(request):
--- backoffice/backoffice/scripts/db.py
+++ backoffice/backoffice/scripts/db.py
@@ -1,9 +1,16 @@
-from django.db import models
 
-def delete_by_filter(model_type: models.Model, **kwargs) -> int:
+def delete_by_filter(model_type, **kwargs) -> int:
     """
     Delete an entry from the given type using filters
     Return the amount of entries that have been deleted
     """
     nb_del, _ = model_type.objects.filter(**kwargs).delete()
+    return nb_del
+
+def delete_all_entries(model_type) -> int:
+    """
+    Delete all the entries in database from the given model
+    Return the total number of entries deleted
+    """
+    nb_del, _ = model_type.objects.all().delete()
     return nb_del
