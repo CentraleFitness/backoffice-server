d329e91783c86f4ee775ecd68af5b8b82467896f
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-09-11 21:00:48 +0200

    new mongo handler

--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@
 Definition of models.
 """
 
-from pymongo import MongoClient
+import logging
+import pymongo
 
 from django.db import models
 from django.utils import timezone
@@ -12,20 +13,39 @@ validate_even = RegexValidator(r"^\w{4}([-]?\w{4}){3}", "La clé doit être de l
 
 # Model for API keys
 
-class MongoClientHandler():
-    def __init__(self, host: str='localhost', port: int=27017):
-        self.client = MongoClient(host, port)
+class MongoHandler():
+    def __init__(self, host: str='localhost', port: int=27017, **kwargs) -> None:
+        self.logger = logging.getLogger()
+        self.client = pymongo.MongoClient(
+            host,
+            port,
+            connect=True,
+            serverSelectionTimeoutMS=3000,
+            connectTimeoutMS=3000,
+            socketTimeoutMS=3000)
+        self.set_db(kwargs.get('db', None))
+        self.set_collection(kwargs.get('collection', None))
 
-class MongoDB(MongoClientHandler):
-    def __init__(self, db: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.db = self.client[db]
+    def set_db(self, db: str):
+        if db:
+            self.db = self.client[db]
+        else:
+            self.db = None
 
-class MongoCollection(MongoDB):
-    def __init__(self, collection: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.collection = self.db[collection]
-    
+    def set_collection(self, collection: str):
+        if self.db and collection:
+            self.collection = self.db[collection]
+        else:
+            self.db = None
+
+    def ping(self) -> bool:
+        try:
+            self.db.command('ping')
+        except pymongo.errors.ServerSelectionTimeoutError:
+            return False
+        return True
+
+
 class apiKey(models.Model):
     api_key = models.CharField(max_length=19, validators=[validate_even])
     date_creation = models.DateTimeField(default=timezone.now)
