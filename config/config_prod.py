7c3b072e563a8c9d2514a2f3e2620d1cdb144fbd
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-09-18 19:21:40 +0200

    fix

--- config/config_prod.py
+++ config/config_prod.py
@@ -2,6 +2,6 @@ from config.default import DefaultConfig
 
 
 class Config(DefaultConfig):
-    ALLOWED_HOSTS.extend(["91.121.155.83", "psyycker.fr.nf"])
+    ALLOWED_HOSTS = ["91.121.155.83", "psyycker.fr.nf", "0.0.0.0", "127.0.0.1"]
     LOG_LEVEL = "INFO"
     DEBUG = False
