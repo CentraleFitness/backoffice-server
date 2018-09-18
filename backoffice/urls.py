a6a6dc5c08c330350a35edd390f517d20512c635
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-09-18 20:03:09 +0200

    quick fix test

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -3,15 +3,17 @@ Definition of urls for backoffice.
 """
 
 from datetime import datetime
-from django.conf.urls import url
-import django.contrib.auth.views
 
 import app.forms
 import app.views
+import django.contrib.auth.views
 
-# Uncomment the next lines to enable the admin:
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include
+from django.conf.urls import url
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = [
@@ -52,4 +54,4 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- config/config_prod.py
+++ config/config_prod.py
@@ -4,4 +4,4 @@ from config.default import DefaultConfig
 class Config(DefaultConfig):
     ALLOWED_HOSTS = ["91.121.155.83", "psyycker.fr.nf", "0.0.0.0", "127.0.0.1"]
     LOG_LEVEL = "INFO"
-    DEBUG = False
+    DEBUG = True
